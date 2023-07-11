3f672e25acc1216ed711ec6cca48a054af5a55e6
Matthew <logicprojectsforfpgas@gmail.com>
2023-07-11 10:23:12 -0600

    basic setup

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+use bevy::prelude::*;
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
+        .run();
+}
+
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
+}
