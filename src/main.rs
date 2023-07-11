8865f5e36ada455873839b7543777ca26fe3697a
Matthew <logicprojectsforfpgas@gmail.com>
2023-07-11 10:58:59 -0600

    basic character movement

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,22 @@ use bevy::prelude::*;
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins(
+            DefaultPlugins
+                .set(ImagePlugin::default_nearest())
+                .set(WindowPlugin {
+                    primary_window: Some(Window {
+                        title: "Logic Farming Rougelike".into(),
+                        resolution: (640.0, 480.0).into(),
+                        resizable: false,
+                        ..default()
+                    }),
+                    ..default()
+                })
+                .build(),
+        )
         .add_systems(Startup, setup)
+        .add_systems(Update, character_movement)
         .run();
 }
 
@@ -20,4 +34,25 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
         texture,
         ..default()
     });
+}
+
+fn character_movement(
+    mut characters: Query<(&mut Transform, &Sprite)>,
+    input: Res<Input<KeyCode>>,
+    time: Res<Time>,
+) {
+    for (mut transform, _) in &mut characters {
+        if input.pressed(KeyCode::W) {
+            transform.translation.y += 100.0 * time.delta_seconds();
+        }
+        if input.pressed(KeyCode::S) {
+            transform.translation.y -= 100.0 * time.delta_seconds();
+        }
+        if input.pressed(KeyCode::D) {
+            transform.translation.x += 100.0 * time.delta_seconds();
+        }
+        if input.pressed(KeyCode::A) {
+            transform.translation.x -= 100.0 * time.delta_seconds();
+        }
+    }
 }
