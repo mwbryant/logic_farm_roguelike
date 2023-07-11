f51eed41647d4de371eae0ca0d864326d2af6b18
Matthew <logicprojectsforfpgas@gmail.com>
2023-07-11 10:40:19 -0600

    blurry character

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,17 @@ fn main() {
         .run();
 }
 
-fn setup(mut commands: Commands) {
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn(Camera2dBundle::default());
+
+    let texture = asset_server.load("character.png");
+
+    commands.spawn(SpriteBundle {
+        sprite: Sprite {
+            custom_size: Some(Vec2::new(100.0, 100.0)),
+            ..default()
+        },
+        texture,
+        ..default()
+    });
 }
