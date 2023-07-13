7eedfd968c4dfba44158360c4b961509d3b46b74
Matthew <logicprojectsforfpgas@gmail.com>
2023-07-13 10:44:13 -0600

    player component

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
 use bevy::prelude::*;
 
+#[derive(Component)]
+pub struct Player {
+    pub speed: f32,
+}
+
 fn main() {
     App::new()
         .add_plugins(
@@ -26,33 +31,38 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
 
     let texture = asset_server.load("character.png");
 
-    commands.spawn(SpriteBundle {
-        sprite: Sprite {
-            custom_size: Some(Vec2::new(100.0, 100.0)),
+    commands.spawn((
+        SpriteBundle {
+            sprite: Sprite {
+                custom_size: Some(Vec2::new(100.0, 100.0)),
+                ..default()
+            },
+            texture,
             ..default()
         },
-        texture,
-        ..default()
-    });
+        Player { speed: 100.0 },
+    ));
 }
 
 fn character_movement(
-    mut characters: Query<(&mut Transform, &Sprite)>,
+    mut characters: Query<(&mut Transform, &Player)>,
     input: Res<Input<KeyCode>>,
     time: Res<Time>,
 ) {
-    for (mut transform, _) in &mut characters {
+    for (mut transform, player) in &mut characters {
+        let movement_amount = player.speed * time.delta_seconds();
+
         if input.pressed(KeyCode::W) {
-            transform.translation.y += 100.0 * time.delta_seconds();
+            transform.translation.y += movement_amount;
         }
         if input.pressed(KeyCode::S) {
-            transform.translation.y -= 100.0 * time.delta_seconds();
+            transform.translation.y -= movement_amount;
         }
         if input.pressed(KeyCode::D) {
-            transform.translation.x += 100.0 * time.delta_seconds();
+            transform.translation.x += movement_amount;
         }
         if input.pressed(KeyCode::A) {
-            transform.translation.x -= 100.0 * time.delta_seconds();
+            transform.translation.x -= movement_amount;
         }
     }
 }
