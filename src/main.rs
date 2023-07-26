70a0ac32ba425c748ac4c22b97a2dcffbdf2cbcc
Matthew <logicprojectsforfpgas@gmail.com>
2023-07-26 08:36:41 -0600

    pig plugin

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,16 @@
 use bevy::{prelude::*, render::camera::ScalingMode};
+use pig::PigPlugin;
 
 #[derive(Component)]
 pub struct Player {
     pub speed: f32,
 }
 
-#[derive(Component)]
-pub struct Pig {
-    pub lifetime: Timer,
-}
-
 #[derive(Resource)]
 pub struct Money(pub f32);
+
+mod pig;
+mod ui;
 
 fn main() {
     App::new()
@@ -30,8 +29,9 @@ fn main() {
                 .build(),
         )
         .insert_resource(Money(100.0))
+        .add_plugins(PigPlugin)
         .add_systems(Startup, setup)
-        .add_systems(Update, (character_movement, spawn_pig, pig_lifetime))
+        .add_systems(Update, character_movement)
         .run();
 }
 
@@ -75,57 +75,6 @@ fn character_movement(
         }
         if input.pressed(KeyCode::A) {
             transform.translation.x -= movement_amount;
-        }
-    }
-}
-
-fn spawn_pig(
-    mut commands: Commands,
-    asset_server: Res<AssetServer>,
-    input: Res<Input<KeyCode>>,
-    mut money: ResMut<Money>,
-    player: Query<&Transform, With<Player>>,
-) {
-    if !input.just_pressed(KeyCode::Space) {
-        return;
-    }
-
-    let player_transform = player.single();
-
-    if money.0 >= 10.0 {
-        money.0 -= 10.0;
-        info!("Spent $10 on a pig, remaining money: ${:?}", money.0);
-
-        let texture = asset_server.load("pig.png");
-
-        commands.spawn((
-            SpriteBundle {
-                texture,
-                transform: *player_transform,
-                ..default()
-            },
-            Pig {
-                lifetime: Timer::from_seconds(2.0, TimerMode::Once),
-            },
-        ));
-    }
-}
-
-fn pig_lifetime(
-    mut commands: Commands,
-    time: Res<Time>,
-    mut pigs: Query<(Entity, &mut Pig)>,
-    mut money: ResMut<Money>,
-) {
-    for (pig_entity, mut pig) in &mut pigs {
-        pig.lifetime.tick(time.delta());
-
-        if pig.lifetime.finished() {
-            money.0 += 15.0;
-
-            commands.entity(pig_entity).despawn();
-
-            info!("Pig sold for $15! Current Money: ${:?}", money.0);
         }
     }
 }
---
+++ src/pig.rs
@@ -0,0 +1,67 @@
+use bevy::prelude::*;
+
+use crate::{Money, Player};
+
+pub struct PigPlugin;
+
+impl Plugin for PigPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(Update, (spawn_pig, pig_lifetime));
+    }
+}
+
+#[derive(Component)]
+pub struct Pig {
+    pub lifetime: Timer,
+}
+
+fn spawn_pig(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    input: Res<Input<KeyCode>>,
+    mut money: ResMut<Money>,
+    player: Query<&Transform, With<Player>>,
+) {
+    if !input.just_pressed(KeyCode::Space) {
+        return;
+    }
+
+    let player_transform = player.single();
+
+    if money.0 >= 10.0 {
+        money.0 -= 10.0;
+        info!("Spent $10 on a pig, remaining money: ${:?}", money.0);
+
+        let texture = asset_server.load("pig.png");
+
+        commands.spawn((
+            SpriteBundle {
+                texture,
+                transform: *player_transform,
+                ..default()
+            },
+            Pig {
+                lifetime: Timer::from_seconds(2.0, TimerMode::Once),
+            },
+        ));
+    }
+}
+
+fn pig_lifetime(
+    mut commands: Commands,
+    time: Res<Time>,
+    mut pigs: Query<(Entity, &mut Pig)>,
+    mut money: ResMut<Money>,
+) {
+    for (pig_entity, mut pig) in &mut pigs {
+        pig.lifetime.tick(time.delta());
+
+        if pig.lifetime.finished() {
+            money.0 += 15.0;
+
+            commands.entity(pig_entity).despawn();
+
+            info!("Pig sold for $15! Current Money: ${:?}", money.0);
+        }
+    }
+}
