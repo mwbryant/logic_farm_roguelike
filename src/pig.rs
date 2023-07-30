4e593cc7be6848f9723d4b613a1dfb252abc6d89
Matthew <logicprojectsforfpgas@gmail.com>
2023-07-30 10:39:12 -0600

    parent and ui

--- src/pig.rs
+++ src/pig.rs
@@ -6,7 +6,8 @@ pub struct PigPlugin;
 
 impl Plugin for PigPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Update, (spawn_pig, pig_lifetime))
+        app.add_systems(Startup, spawn_pig_parent)
+            .add_systems(Update, (spawn_pig, pig_lifetime))
             .register_type::<Pig>();
     }
 }
@@ -17,18 +18,27 @@ pub struct Pig {
     pub lifetime: Timer,
 }
 
+#[derive(Component)]
+pub struct PigParent;
+
+fn spawn_pig_parent(mut commands: Commands) {
+    commands.spawn((SpatialBundle::default(), PigParent, Name::new("Pig Parent")));
+}
+
 fn spawn_pig(
     mut commands: Commands,
     asset_server: Res<AssetServer>,
     input: Res<Input<KeyCode>>,
     mut money: ResMut<Money>,
     player: Query<&Transform, With<Player>>,
+    parent: Query<Entity, With<PigParent>>,
 ) {
     if !input.just_pressed(KeyCode::Space) {
         return;
     }
 
     let player_transform = player.single();
+    let parent = parent.single();
 
     if money.0 >= 10.0 {
         money.0 -= 10.0;
@@ -36,17 +46,19 @@ fn spawn_pig(
 
         let texture = asset_server.load("pig.png");
 
-        commands.spawn((
-            SpriteBundle {
-                texture,
-                transform: *player_transform,
-                ..default()
-            },
-            Pig {
-                lifetime: Timer::from_seconds(2.0, TimerMode::Once),
-            },
-            Name::new("Pig"),
-        ));
+        commands.entity(parent).with_children(|commands| {
+            commands.spawn((
+                SpriteBundle {
+                    texture,
+                    transform: *player_transform,
+                    ..default()
+                },
+                Pig {
+                    lifetime: Timer::from_seconds(2.0, TimerMode::Once),
+                },
+                Name::new("Pig"),
+            ));
+        });
     }
 }
 
@@ -54,14 +66,18 @@ fn pig_lifetime(
     mut commands: Commands,
     time: Res<Time>,
     mut pigs: Query<(Entity, &mut Pig)>,
+    parent: Query<Entity, With<PigParent>>,
     mut money: ResMut<Money>,
 ) {
+    let parent = parent.single();
+
     for (pig_entity, mut pig) in &mut pigs {
         pig.lifetime.tick(time.delta());
 
         if pig.lifetime.finished() {
             money.0 += 15.0;
 
+            commands.entity(parent).remove_children(&[pig_entity]);
             commands.entity(pig_entity).despawn();
 
             info!("Pig sold for $15! Current Money: ${:?}", money.0);
--- src/ui.rs
+++ src/ui.rs
@@ -15,13 +15,36 @@ impl Plugin for GameUI {
 }
 
 fn spawn_game_ui(mut commands: Commands) {
-    commands.spawn((
-        TextBundle {
-            text: Text::from_section("Money!", TextStyle::default()),
-            ..default()
-        },
-        MoneyText,
-    ));
+    commands
+        .spawn((
+            NodeBundle {
+                style: Style {
+                    width: Val::Percent(100.0),
+                    height: Val::Percent(10.0),
+                    align_items: AlignItems::Center,
+                    padding: UiRect::all(Val::Px(10.0)),
+                    ..default()
+                },
+                background_color: Color::BLUE.into(),
+                ..default()
+            },
+            Name::new("UI Root"),
+        ))
+        .with_children(|commands| {
+            commands.spawn((
+                TextBundle {
+                    text: Text::from_section(
+                        "Money!",
+                        TextStyle {
+                            font_size: 32.0,
+                            ..default()
+                        },
+                    ),
+                    ..default()
+                },
+                MoneyText,
+            ));
+        });
 }
 
 fn update_money_ui(mut texts: Query<&mut Text, With<MoneyText>>, money: Res<Money>) {
