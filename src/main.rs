57c1c3e9de9d75349ecda94a7213e04023fe7e99
Matthew <logicprojectsforfpgas@gmail.com>
2023-08-19 18:25:10 -0600

    better resolution for videos

--- src/main.rs
+++ src/main.rs
@@ -28,7 +28,7 @@ fn main() {
                 .set(WindowPlugin {
                     primary_window: Some(Window {
                         title: "Logic Farming Roguelike".into(),
-                        resolution: (640.0, 480.0).into(),
+                        resolution: (853.3, 480.0).into(),
                         resizable: false,
                         ..default()
                     }),
